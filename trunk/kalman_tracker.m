5b81034d84fd2e954ab23a04a91dccd21761a4a3
a.mounir86 <a.mounir86@cd617d9e-278e-11df-ba5e-8b7f734aaaf9>
2010-03-17 23:05:00 +0000

    Presentation another part

--- trunk/kalman_tracker.m
+++ trunk/kalman_tracker.m
@@ -5,7 +5,7 @@ Segmenter.tau     = tau;
 Segmenter.radius  = radius;
 Segmenter.segment = @background_subtractor;
 
-% Recognizer and representer is a simple blob finder.
+%% Detector
 Recognizer.recognize = @find_blob;
 Representer.represent = @filter_blobs2;
 
@@ -20,10 +20,12 @@ Tracker.R          = 5 * eye(6);  % Measurement noise
 Tracker.innovation = 0;
 Tracker.track      = @kalman_step;
 
+%% Visualizer
 % A custom visualizer for the Kalman state.
 Visualizer.visualize = @visualize_kalman;
 Visualizer.paused    = false;
 
+%% Initialize
 % Set up the global tracking system.
 T.segmenter   = Segmenter;
 T.recognizer  = Recognizer;
@@ -31,6 +33,7 @@ T.representer = Representer;
 T.tracker     = Tracker;
 T.visualizer  = Visualizer;
 
+%% Execute
 % And run the tracker on the video.
 run_tracker(fname, T);
 return
